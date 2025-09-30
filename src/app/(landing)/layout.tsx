403d341e3a1a266fb35fcd093b3ea9a1dc104119
HoangDucBach <hoangbach0985@gmail.com>
2025-09-30 22:10:55 +0700

    fix(lint): full

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,5 +1,11 @@
 import { Container, VStack } from "@chakra-ui/react";
 import { Header } from "./_components/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Firyx - Decentralized Lending Protocol",
+    description: "Firyx is a protocol for collateral-free lending designed to provide hybrid earnings safely.",
+};
 
 export default async function Layout({
     children,
--- src/app/app/loan-positions/[id]/layout.tsx
+++ src/app/app/loan-positions/[id]/layout.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from "next";
+
+export async function generateMetadata({
+    params,
+}: {
+    params: { id: string }
+}): Promise<Metadata> {
+    const { id } = params;
+
+    return {
+        title: `Loan Position ${id} - Firyx`,
+        description: `View and manage your loan position, on Firyx.`,
+    }
+}
 
 export default async function Layout({
     children,
