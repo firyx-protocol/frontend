cd9094c102ce6c74d8dae91aaea938d9229c51e8
HoangDucBach <hoangbach0985@gmail.com>
2025-10-01 20:34:38 +0700

    fix(release): ui

--- src/app/app/_components/Header.tsx
+++ src/app/app/_components/Header.tsx
@@ -1,23 +1,25 @@
 "use client";
 
-import { chakra, HStack, HtmlProps } from "@chakra-ui/react";
+import { Button, chakra, HStack, HtmlProps } from "@chakra-ui/react";
 import { Logo } from "../../../components/brands/Logo";
 import { AccountControl } from "@/app/app/_components/AccountControl";
-import { LinkButton } from "@/components/ui/link-button";
 import { MenuControl } from "./MenuControl";
+import { useRouter } from "next/navigation";
 
 type HeaderProps = HtmlProps;
 
 export const Header = (props: HeaderProps) => {
+    const router = useRouter();
+
     return (
         <chakra.header w={"full"} p={"2"} {...props} >
             <HStack justifyContent={"space-between"} direction={"row"} alignItems={"center"} width={"full"} height={"full"}>
-                <LinkButton
+                <Button
                     variant={"plain"}
-                    href={"/"}
+                    onClick={() => router.push("/")}
                 >
                     <Logo />
-                </LinkButton>
+                </Button>
                 <HStack justify={"end"} gap={"4"}>
                     <MenuControl />
                     <AccountControl />
